0f9dad25901a33b6d3ceddda74cf87ee590ff472
blackscure <weksabuyahi@gmail.com>
2025-09-04 06:06:48 +0300

    urls created

---
+++ store/urls.py
@@ -0,0 +1,13 @@
+# store/urls.py
+from django.urls import path
+from . import views
+
+app_name = 'store'
+
+urlpatterns = [
+    path('', views.product_list, name='product_list'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('cart/', views.cart_detail, name='cart_detail'),
+    path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/remove/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
+]
